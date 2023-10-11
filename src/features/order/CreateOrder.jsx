a87354f35de5f5c11e243a3026aca55a579bd5b8
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-12 01:57:28 +0200

    Transitions

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,33 +1,33 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -38,8 +38,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
-  
-  
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -53,7 +52,6 @@ function CreateOrder() {
           <label>First Name</label>
           <input type="text" name="customer" required />
         </div>
-
 
         <div>
           <label>Phone number</label>
@@ -83,8 +81,11 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing order" : "Order now"}
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full disabled:cursor-not-allowed bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2"
+          >
+            {isSubmitting ? 'Placing order' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -92,7 +93,7 @@ function CreateOrder() {
   );
 }
 
-export async function action({request}){
+export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
   console.log(data);
@@ -101,14 +102,15 @@ export async function action({request}){
     ...data,
     cart: JSON.parse(data.cart),
     priority: data.priority === 'on',
-  }
+  };
 
   const errors = {};
-  if(!isValidPhone(order.phone)){
-    errors.phone = 'Please give us your correct phone number. We might need it to contact you.'
+  if (!isValidPhone(order.phone)) {
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
   }
 
-  if(Object.keys(errors).length > 0) return errors;
+  if (Object.keys(errors).length > 0) return errors;
 
   //If no error, create new order and redirect
   const newOrder = await createOrder(order);
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,11 +9,11 @@ function AppLayout() {
   //console.log(navigation);
   const isLoading = navigation.state === 'loading';
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-gray-800">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
       <div className=" ">
-        <main className="mx-auto w-96 bg-gray-500">
+        <main className="mx-auto w-96 ">
           <Outlet />
         </main>
       </div>
