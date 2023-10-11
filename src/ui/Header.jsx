51ff82851da003094de8aed0dfd8a5992971c9d8
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-11 04:51:25 +0200

    Tailwind Install

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "../features/order/SearchOrder"
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
-    return (
-        <header>
-            <Link to='/' className="text-yellow-500">Fast React Pizza Co.</Link>  
-            <SearchOrder/> 
-            <p>Alaa</p>         
-        </header>
-    )
+  return (
+    <header>
+      <Link to="/" className="p-2 font-bold text-yellow-500">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <p>Alaa</p>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
