3b4fcb792784e59241580691115d6ec677e3db22
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-12 01:12:06 +0200

    Grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,26 +1,26 @@
 /* eslint-disable no-unused-vars */
-import Header from "./Header"
+import Header from './Header';
 import CartOverview from './../features/cart/CartOverview';
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
-
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 function AppLayout() {
-    const navigation = useNavigation();
-    //console.log(navigation);
-    const isLoading = navigation.state === 'loading';
-    return (
-        <div className="layout">
-            {isLoading && <Loader/>}
-            <Header/>
+  const navigation = useNavigation();
+  //console.log(navigation);
+  const isLoading = navigation.state === 'loading';
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-gray-800">
+      {isLoading && <Loader />}
+      <Header />
+      <div className=" ">
+        <main className="mx-auto w-96 bg-gray-500">
+          <Outlet />
+        </main>
+      </div>
 
-            <main>
-                <Outlet/>
-            </main>
-
-            <CartOverview/>
-        </div>
-    )
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
