313245dc6873f43fa803f0b754b8341ccd9d6cb4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-12 02:17:02 +0200

    Form

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -14,7 +14,7 @@ function SearchOrder() {
     
     return (
         <form onSubmit={handleSubmit}>
-            <input placeholder="Search order #" value={query} onChange={(e) => setQuery(e.target.value)}/>
+            <input className="rounded-full px-4 py-2 text-sm bg-yellow-100 focus:outline-none w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:ring focus:ring-yellow-500 placeholder:text-stone-400" placeholder="Search order #" value={query} onChange={(e) => setQuery(e.target.value)}/>
         </form>
     )
 }
