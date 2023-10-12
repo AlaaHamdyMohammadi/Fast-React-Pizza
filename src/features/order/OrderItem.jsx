9988ec1f84db80b0ecc9d63375b391efad99b255
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-12 03:20:45 +0200

    ReuseComponent

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
