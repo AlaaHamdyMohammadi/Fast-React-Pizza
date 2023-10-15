79f3c010c719b74b15a903d959854c273ac68534
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-15 23:45:39 +0200

    Delete Cart Item

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -4,14 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import {formatCurrency} from '../../utils/helpers'
 import { deleteItem, getTotalCartQuantity } from './cartSlice';
+import DeleteItem from './DeleteItem';
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-  // const cartQuantity = useSelector(getTotalCartQuantity);
-  // const cartName = useSelector((state) => state.cart.cart.name);
-  const dispatch = useDispatch();
-  function handleDeleteItem(){
-    dispatch(deleteItem(pizzaId))
-  }
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -20,7 +15,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small" onClick={handleDeleteItem}>Delete</Button>
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button"
+import { deleteItem } from "./cartSlice";
+
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+  function handleDeleteItem() {
+    dispatch(deleteItem(pizzaId));
+  }
+  return (
+    <Button type="small" onClick={handleDeleteItem}>
+      Delete
+    </Button>
+  );
+}
+
+export default DeleteItem
