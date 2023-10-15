1d5f208d2fcb74f8e0ce615cdb3ed1986446bb14
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-10-15 21:36:35 +0200

    Handle Cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,36 +3,23 @@ import { Link } from 'react-router-dom';
 import Button from '../../ui/Button';
 import LinkButton from '../../ui/LinkButton';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from './cartSlice';
+import EmptyCart from './EmptyCart'
 
 function Cart() {
-  const cart = fakeCart;
+  //const cart = fakeCart;
   const username = useSelector((state) => state.user.username);
-  
+  const cart = useSelector(state => state.cart.cart);
+  const dispatch = useDispatch();
+
+  function handleClearCart(){
+    dispatch(clearCart())
+  }
+
+  if(cart.length === 0){
+    return <EmptyCart/>
+  }
   return (
     <div className="px-4 py-3 ">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -47,7 +34,7 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={handleClearCart}>Clear cart</Button>
       </div>
     </div>
   );
--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
 import {formatCurrency} from '../../utils/helpers'
+import { deleteItem, getTotalCartQuantity } from './cartSlice';
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  // const cartQuantity = useSelector(getTotalCartQuantity);
+  // const cartName = useSelector((state) => state.cart.cart.name);
+  const dispatch = useDispatch();
+  function handleDeleteItem(){
+    dispatch(deleteItem(pizzaId))
+  }
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -12,7 +20,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <Button type="small" onClick={handleDeleteItem}>Delete</Button>
       </div>
     </li>
   );
